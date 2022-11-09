1934ea41270ac763032cb604f37cf01f332c3edc
Aminul Islam Rocky <mairoky@outlook.com>
2022-11-09 18:11:50 +0600

    mongodb connection setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -12,6 +14,24 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is Running!');
 })
+
+{/* mongodb+srv://db_admin:<password>@cluster0.v3j0rcs.mongodb.net/test  */ }
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v3j0rcs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('service_review').collection('services');
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(e => console.error(e));
+
 
 app.listen(port, () => {
     console.log(`Server is Running on PORT:${port}`);
